36e7ab3317aff0c5b3f81f161a12a2777a765186
mkralik3 <mkralik@redhat.com>
2023-10-03 15:40:08 +0200

    [go] Add chi lib for router

--- goOrdersApi/go.mod
+++ goOrdersApi/go.mod
@@ -1,3 +1,5 @@
 module github.com/mkralik3/Learning/goOrdersApi
 
 go 1.20
+
+require github.com/go-chi/chi/v5 v5.0.10
--- goOrdersApi/main.go
+++ goOrdersApi/main.go
@@ -3,13 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-
+	router := chi.NewRouter()
+	router.Get("/hello", basicHandler)
+	
 	server := &http.Server{
 		Addr:    ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err := server.ListenAndServe()
