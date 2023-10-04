3016d973cf9a939c9f44a43edb8805fec95415cb
mkralik3 <mkralik@redhat.com>
2023-10-04 10:10:02 +0200

    [go] Add data models

---
+++ goOrdersApi/README.md
@@ -0,0 +1,10 @@
+Project for this tutorial: https://www.youtube.com/playlist?list=PL4cUxeGkcC9iImF8w9FbFOc2UntutL9Wv
+
+Redis DB:
+```
+docker run -p 6379:6379 redis:latest
+```
+Redis cli
+```
+redis-cli
+```
--- goOrdersApi/main.go
+++ goOrdersApi/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
+	"os/signal"
 
 	"github.com/mkralik3/Learning/goOrdersApi/application"
 )
@@ -10,10 +12,12 @@ import (
 func main() {
 	app := application.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 
 	if err != nil {
 		fmt.Println("failed to start app:", err)
 	}
-
 }
